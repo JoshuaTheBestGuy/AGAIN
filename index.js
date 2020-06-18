4d96cfc9760e71b1b0569d05dce96e7e195cf1ac
joshua <jhendrickson2006@gmail.com>
2020-06-17 21:03:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Discord = require("discord.js")
 const fs = require("fs")
 const client = new Discord.Client()
 
+require("./util/eventHandler")(bot)
+
 fs.readdir("./events/", (err, files) => {
   files.forEach(file => {
     const eventHandler = require(`./events/${file}`)
