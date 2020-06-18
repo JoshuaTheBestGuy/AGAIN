697e8905fabd9e00bf5ced51582e3793a7aa426c
joshua <jhendrickson2006@gmail.com>
2020-06-18 13:49:39 -0700

    yesyeysyes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ fs.readdir("./commands/", (err, files) => {
 
     jsfile.forEach((f, i) => {
         let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);
+        client.commands.set(pull.config.name, pull);
         pull.config.aliases.forEach(alias => {
             bot.aliases.set(alias, pull.config.name)
         });
