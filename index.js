4e88c9b0eb3900767ac4ea0b41fc06166a40cc88
joshua <jhendrickson2006@gmail.com>
2020-06-18 11:57:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ client.login(process.env.token);
 
 // Welcome and Leave
 
-const channel = member.guild.channels.cache.find(ch => ch.id === '715639424989528226');
-    if (!channel) return;
+const welcomechannel = member.guild.channels.cache.find(ch => ch.id === '715639424989528226');
+    if (!welcomechannel) return;
     channel.send (`Welcome to Isaiah Incorporatations ${member}, We wish you enjoy stay (because you wont be leaving)`)
 
 const channel = member.guild.channels.cache.find(ch => ch.id === '715639460091527179');
