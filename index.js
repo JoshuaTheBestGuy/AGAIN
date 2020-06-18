b58aabb2e9d313ad093d058aaee70f2c58c99f90
joshua <jhendrickson2006@gmail.com>
2020-06-18 10:20:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ fs.readdir("./events/", (err, files) => {
   })
 })
 
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
