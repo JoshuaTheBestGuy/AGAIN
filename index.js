23e73d9abb53c71b3b4d77036ae852f2d87d2a23
joshua <jhendrickson2006@gmail.com>
2020-06-18 10:25:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -69,3 +69,10 @@ client.on("message", message => {
 })
 // ...
 client.login(process.BOT_TOKEN)
+
+
+client.on('message', msg => {
+ if (msg.content === 'isaiah') {
+ msg.reply('You called?');
+ }
+ });
