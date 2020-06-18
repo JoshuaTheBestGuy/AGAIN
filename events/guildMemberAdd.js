e3577ffb63b30099ea458c8d393e703b573080c1
joshua <jhendrickson2006@gmail.com>
2020-06-17 20:45:06 -0700

    First

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = (client, member) => {
+  member.send(
+    `Welcome to Isaiah Incorporatations, We wish you enjoy stay (because you wont be leaving)`
+  )
+}
---
+++ index.js
@@ -0,0 +1,72 @@
+// Nerd Stuff
+
+require("dotenv").config()
+const Discord = require("discord.js")
+const fs = require("fs")
+const client = new Discord.Client()
+
+fs.readdir("./events/", (err, files) => {
+  files.forEach(file => {
+    const eventHandler = require(`./events/${file}`)
+    const eventName = file.split(".")[0]
+    client.on(eventName, arg => eventHandler(client, arg))
+  })
+})
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+ });
+
+client.on('message', msg => {
+ if (msg.content === 'ping') {
+ msg.reply('pong');
+ }
+ });
+
+client.login(process.env.token);
+
+require("dotenv").config()
+// ...
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!")
+  }
+})
+client.on("guildMemberAdd", member => {
+  member.send(
+    `Welcome to Isaiah Incorporatations, We wish you enjoy stay (because you wont be leaving)`
+  )
+})
+client.login(process.BOT_TOKEN)
+
+// Kicking
+
+require("dotenv").config()
+// ...
+client.on("message", message => {
+  if (message.content.startsWith("!kick")) {
+    const member = message.mentions.members.first()
+    if (!member) {
+      return message.reply(
+        `Who are you trying to kick? You must mention a user.`
+      )
+    }
+    if (!member.kickable) {
+      return message.reply(`I can't kick this user. Sorry!`)
+    }
+    return member
+      .kick()
+      .then(() => message.reply(`${member.user.tag} was kicked.`))
+      .catch(error => message.reply(`Sorry, an error occured.`))
+  }
+})
+// ...
+client.login(process.BOT_TOKEN)
+
+// Custom Messages
+
+client.on('message', msg => {
+ if (msg.content === 'isaiah') {
+ msg.reply('You called?');
+ }
+ });
